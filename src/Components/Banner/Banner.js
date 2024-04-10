032301085f4c6cff08d7dc3c9da1dbfcd40ad946
Haritha944 <harithams23@gmail.com>
2024-04-10 11:23:19 +0530

    add content

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Banner.css';
+import Arrow from '../../assets/Arrow'
+
+
+const Banner = () => {
+  return (
+  <div className='bannerParentDiv'>
+    <div className='bannerChildDiv'>
+        <div className='menuBar'>
+            <div className='categoryMenu'>
+                <span>ALL CATEGORIES</span>
+                <Arrow></Arrow>
+            </div>
+            <div className='otherQuickOptions'>
+                <span>Cars</span>
+                <span>Motorcycle</span>
+                <span>Mobile Phone</span>
+                <span>Commercial & other vehicle</span>
+                <span>For Sale:House and Apartments</span>
+            </div>
+        </div>
+        <div className='banner'>
+        <img
+            src="../../../Images/banner copy.png"
+            alt=""
+          />
+        </div>
+    </div>
+  </div>
+  );
+}
+
+export default Banner;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../Components/Banner/Banner';
+
+function Home(props){
+  return (
+    <div className='homeParentDiv'>
+        <Banner />
+        </div>
+  );
+}
+
+export default Home;
